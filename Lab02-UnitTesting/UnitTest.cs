725b00380d28e206cab61039eea26922d4cad63b
ConnerKT <connerthompson121@gmail.com>
2023-07-11 22:51:17 -0500

    added lab 2 changes

---
+++ Lab02-UnitTesting/UnitTest.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace UnitTest
+{
+
+    public class Lab2Tests
+    {
+        [Theory]
+        [InlineData("12")]
+        [InlineData("22")]
+
+        public void ViewBalanceTest(string balance)
+        {
+            var deciamalBalance = Convert.ToDecimal(balance);
+            Assert.Equal(deciamalBalance, Program.ViewBalanceTest(deciamalBalance));
+        }
+    }
